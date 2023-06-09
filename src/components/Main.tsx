20b18fb257d02790d17719510b06c7ba3598ae21
Spyei <caio.salamao1234@gmail.com>
2023-06-09 19:15:22 -0300

    video :skull:

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import video from "../assets/video.mp4";
 import { Simbolo } from "./Simbolo";
 import { Caracteristicas } from "./Caracteristicas";
 
@@ -10,24 +9,28 @@ export const Main: React.FC = () => {
                 <div className="w-[100vw] flex justify-center items-center flex-col">
                     <div className="w-[80vw] h-[250px] flex justify-center items-center">
                         <h1 className="flex justify-center items-center flex-col">
-                            <strong className="text-[40px] xl:text-[28px]" id="oqueé">
+                            <strong
+                                className="text-[40px] xl:text-[28px]"
+                                id="oqueé"
+                            >
                                 Oque é o Autismo?
                             </strong>
-                            <span className='xl:text-[13px]'>
+                            <span className="xl:text-[13px]">
                                 Veja o video abaixo de perguntas sobre o
                                 autismo, pela vice-diretora da escola.
                             </span>
                         </h1>
                     </div>
                     <div className="bg-neutral-300 xl:w-[100vw] w-[50vw] justify-center flex items-center rounded-xl">
-                        <video className="p-4" controls>
-                            <source src={video} type="video/mp4" />
-                        </video>
+                        <iframe
+                            className="p-4 xl:w-[100vw] xl:h-[300px] h-[400px] w-[50vw]"
+                            src="https://www.youtube.com/embed/2a50HuHlD0I"
+                        ></iframe>
                     </div>
                 </div>
             </div>
             <Simbolo />
-            <Caracteristicas/>
+            <Caracteristicas />
         </main>
     );
 };
